616038da22be9d5783a9beda24703155063fc9c7
Steven Armstrong <steven.armstrong@id.ethz.ch>
2015-04-28 13:45:41 +0200

    initial working version of `config` subcommand
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- cdist/cli/commands/config.py
+++ cdist/cli/commands/config.py
@@ -1,10 +1,12 @@
 import sys
 import os
 import tempfile
+import asyncio
 
 import click
 
 from cdist import session
+from cdist import runtime
 from cdist.cli.utils import comma_delimited_string_to_set
 
 
@@ -48,13 +50,42 @@ def main(ctx, manifest, only_tag, include_tag, exclude_tag, dry_run, operation_m
         ctx.fail('Options \'include-tag\' and \'exclude-tag\' have conflicting values: %s vs %s' % (include_tag, exclude_tag))
 
     _session = session.Session()
+    _session.add_conf_dir(os.path.expanduser('~/vcs/cdist/cdist/conf'))
     _session.add_conf_dir(os.path.expanduser('~/.cdist'))
     _session.add_conf_dir(os.path.expanduser('~/vcs/cdist-ng/conf'))
+
+    # override remote-cache-dir for testing
+    _remote_session_dir = tempfile.mkdtemp(prefix='cdist-remote-')
+    _session['remote-cache-dir'] = os.path.join(_remote_session_dir, _session['session-id'])
+
+
     #url = 'ssh+sudo+chroot://root@netboot-dev.ethz.ch/local/nfsroot/preos'
     for url in target:
-        # TODO: test and normalize into valid url that urllib understands
         _session.add_target(url)
 
     _session_dir = tempfile.mkdtemp(prefix='cdist-session-')
     print(_session_dir)
     _session.to_dir(_session_dir)
+
+
+    loop = asyncio.get_event_loop()
+    tasks = []
+
+    for _target in _session.targets:
+        _runtime = runtime.Runtime(_session, _target, _session_dir)
+        loop.run_until_complete(_runtime.run_global_explorers())
+        loop.run_until_complete(_runtime.run_initial_manifest())
+
+        for _object in _runtime.list_objects():
+            loop.run_until_complete(_runtime.run_type_explorers(_object))
+            loop.run_until_complete(_runtime.run_type_manifest(_object))
+            _object['code-local'] = loop.run_until_complete(_runtime.run_gencode_local(_object))
+            _object['code-remote'] = loop.run_until_complete(_runtime.run_gencode_remote(_object))
+            _runtime.sync_object(_object)
+            if _object['code-local']:
+               loop.run_until_complete(_runtime.run_code_local(_object))
+            if _object['code-remote']:
+               loop.run_until_complete(_runtime.transfer_code_remote(_object))
+               loop.run_until_complete(_runtime.run_code_remote(_object))
+
+    loop.close()
