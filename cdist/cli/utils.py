455e54fbff8b887ed1f81481bde181c8d31f4a43
Steven Armstrong <steven.armstrong@id.ethz.ch>
2015-05-13 15:36:03 +0200

    add utitlity to flatten space delimited string to set
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- cdist/cli/utils.py
+++ cdist/cli/utils.py
@@ -1,11 +1,21 @@
 
-def comma_delimited_string_to_set(ctx, param, value):
-    """A click option callback that flattens a list of comma seperated strings
-    into one list of options.
+class delimited_string_to_set(object):
+    """A click option callback that flattens a list of delimiter seperated
+    strings into one list of options.
+
+    Usage:
+
+    comma_delimited_string_to_set = delimited_string_to_set(',')
+    option = click.Option(('--name',), callback=comma_delimited_string_to_set)
     """
-    _list = []
-    for v in value:
-        _list.extend(v.split(','))
-    return set(_list)
+    def __init__(self, delimiter):
+        self.delimiter = delimiter
 
+    def __call__(self, ctx, param, value):
+        _list = []
+        for v in value:
+            _list.extend(v.split(self.delimiter))
+        return set(_list)
 
+comma_delimited_string_to_set = delimited_string_to_set(',')
+space_delimited_string_to_set = delimited_string_to_set(' ')
