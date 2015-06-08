e4f6b1d8554616fcf1acc9fa1b754e9a162be339
Steven Armstrong <steven.armstrong@id.ethz.ch>
2015-06-08 22:39:54 +0200

    add support for profiling
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- cdist/cli/__init__.py
+++ cdist/cli/__init__.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import itertools
 import logging
 
 import click
@@ -9,6 +10,50 @@ from click.utils import make_str
 commands_folder = os.path.join(os.path.dirname(__file__), 'commands')
 internal_commands_folder = os.path.join(os.path.dirname(__file__), 'commands', 'internal')
 
+
+def get_profile_path(path):
+    profile_path = path
+    if os.path.exists(profile_path):
+        for suffix in itertools.count(1):
+            possible_profile_path = '{0}_{1}'.format(profile_path, suffix)
+            if not os.path.exists(possible_profile_path):
+                profile_path = possible_profile_path
+                break
+    return profile_path
+
+
+def do_cprofile(func):
+    if 'CDIST_PROFILE' in os.environ:
+        import cProfile
+        def profiled_func(*args, **kwargs):
+            profile_dir = os.path.expanduser('~/tmp/cdist-profile')
+            prog_name = make_str(os.path.basename(
+                    sys.argv and sys.argv[0] or __file__))
+            if prog_name.startswith('__'):
+                profile_file = [prog_name]
+                if len(sys.argv) > 1:
+                    maybe_object_id = sys.argv[1]
+                    if not maybe_object_id.startswith('--'):
+                        object_id = maybe_object_id
+                        profile_file.append(object_id.replace('/', '#'))
+                profile_file = '-'.join(profile_file)
+            else:
+                profile_file = prog_name
+            profile_path = get_profile_path(os.path.join(profile_dir, profile_file))
+            print('profile_path: '+ profile_path)
+            #profile = cProfile.run('runit()', profile_path)
+            profile = cProfile.Profile()
+            try:
+                profile.enable()
+                result = func(*args, **kwargs)
+                profile.disable()
+                return result
+            finally:
+                #profile.print_stats()
+                profile.dump_stats(profile_path)
+        return profiled_func
+    else:
+        return func
 
 class SubfolderMultiCommand(click.MultiCommand):
     '''Loads subcommands from subfolder.
@@ -54,6 +99,7 @@ class SubfolderMultiCommand(click.MultiCommand):
         return super().main(**kwargs)
 
 
+@do_cprofile
 @click.command(cls=SubfolderMultiCommand)
 @click.option('--verbose', '-v', 'log_level', flag_value='info', help='set log level to info', envvar='__cdist_log_level')
 @click.option('--debug', '-d', 'log_level', flag_value='debug', help='set log level to debug', envvar='__cdist_log_level')
