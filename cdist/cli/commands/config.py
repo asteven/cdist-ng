a892e5c48295e8b49a2cc98441b6ffa6a547376e
Steven Armstrong <steven.armstrong@id.ethz.ch>
2015-06-18 23:22:13 +0200

    remove DependencyResolver
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- cdist/cli/commands/config.py
+++ cdist/cli/commands/config.py
@@ -6,84 +6,25 @@ import pprint
 
 import click
 
+from cdist import exceptions
 from cdist import session
 from cdist import runtime
-from cdist import dependency
+from cdist import manager
 
 from cdist.cli.utils import comma_delimited_string_to_set
 
 
-@asyncio.coroutine
-def prepare_object(_runtime, _object):
-    _runtime.log.info('Prepare object: %s', _object)
-    yield from _runtime.run_type_explorers(_object)
-    yield from _runtime.run_type_manifest(_object)
-    _object['state'] = _runtime.OBJECT_PREPARED
-    # FIXME: no point for disk roundtrip here
-    _runtime.sync_object(_object)
-
-
-@asyncio.coroutine
-def run_object(_runtime, _object):
-    _runtime.log.info('Run object: %s', _object)
-    _object['code-local'] = yield from _runtime.run_gencode_local(_object)
-    _object['code-remote'] = yield from _runtime.run_gencode_remote(_object)
-    _runtime.sync_object(_object)
-    if _object['code-local']:
-       yield from _runtime.run_code_local(_object)
-    if _object['code-remote']:
-       yield from _runtime.transfer_code_remote(_object)
-       yield from _runtime.run_code_remote(_object)
-    _object['state'] = _runtime.OBJECT_DONE
-    # FIXME: no point for disk roundtrip here
-    _runtime.sync_object(_object)
-
-
-@asyncio.coroutine
-def stage_prepare(_runtime):
-    # Continue processing until no more new objects are created
-    new_objects_created = True
-    while new_objects_created:
-        new_objects_created = False
-        for _object in _runtime.list_objects():
-            deps = _runtime.get_dependencies(_object)
-            # Skip objects that have a 'require' dependencies as they can not
-            # be prepared yet.
-            if deps['require']:
-                continue
-
-            if _object['state'] == _runtime.OBJECT_PREPARED:
-                _runtime.log.info('Skipping re-prepare of object %s', _object)
-                continue
-            else:
-                yield from prepare_object(_runtime, _object)
-                # Preparing the object could have created new objects
-                new_objects_created = True
-
-
-@asyncio.coroutine
-def stage_run(_runtime):
-    objects = _runtime.list_objects()
-    resolver = dependency.DependencyResolver(objects, _runtime.dependency)
-    _runtime.log.debug(pprint.pformat(resolver.requirements))
-    _runtime.log.debug(pprint.pformat(resolver.dependencies))
-
-    for _object in resolver:
-        if _object['state'] == _runtime.OBJECT_PREPARED:
-            yield from run_object(_runtime, _object)
-        else:
-            # FIXME: new objects created by prepare_object are not known to the resolver
-            yield from prepare_object(_runtime, _object)
-            yield from run_object(_runtime, _object)
-
 
 @asyncio.coroutine
 def configure_target(_runtime):
+    _runtime.log.info('configure_target')
     yield from _runtime.initialize()
     yield from _runtime.run_global_explorers()
     yield from _runtime.run_initial_manifest()
-    yield from stage_prepare(_runtime)
-    yield from stage_run(_runtime)
+
+    om = manager.ObjectManager(_runtime)
+    yield from om.process()
+
     return _runtime
 
 
@@ -155,7 +96,15 @@ def main(ctx, manifest, only_tag, include_tag, exclude_tag, dry_run, operation_m
 
     # Execute the tasks in parallel using asyncio.
     loop = asyncio.get_event_loop()
-    results = []
-    if tasks:
-        results = loop.run_until_complete(asyncio.gather(*tasks))
-    loop.close()
+
+    try:
+        results = []
+        if tasks:
+            results = loop.run_until_complete(asyncio.gather(*tasks))
+    except exceptions.CdistError as e:
+        log.error(str(e))
+        raise
+        ctx.exit(1)
+    finally:
+        loop.close()
+
