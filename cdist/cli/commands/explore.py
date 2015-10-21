fd45ce52fe22a2756198dd81f051ed2ee61a2668
Steven Armstrong <steven.armstrong@id.ethz.ch>
2015-10-21 22:22:53 +0200

    make `cdist explore` actually work with remote hosts
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- cdist/cli/commands/explore.py
+++ cdist/cli/commands/explore.py
@@ -7,7 +7,10 @@ import asyncio.subprocess
 
 import click
 
+from cdist import exceptions
 from cdist import session
+from cdist import runtime
+
 from cdist.cli.utils import comma_delimited_string_to_set
 
 
@@ -72,25 +75,45 @@ def main(ctx, explorer, target):
     _session.add_conf_dir(os.path.expanduser('~/vcs/cdist/cdist/conf'))
     _session.add_conf_dir(os.path.expanduser('~/.cdist'))
     _session.add_conf_dir(os.path.expanduser('~/vcs/cdist-ng/conf'))
-    if target is not '__local__':
-        _session.add_target(target)
 
-    _session_dir = tempfile.mkdtemp(prefix='cdist-session-')
-    log.debug('session_dir: {0}'.format(_session_dir))
-    _session.to_dir(_session_dir)
-
-    explorer_path = os.path.join(_session_dir, 'conf/explorer')
-    os.environ['__explorer'] = explorer_path
+    local_session_dir = tempfile.mkdtemp(prefix='cdist-session-')
 
     loop = asyncio.get_event_loop()
-    mode = 'shell'
-    tasks = []
-    for name,executable in _session['conf']['explorer'].items():
-        if explorer and name not in explorer:
-            continue
-        task = asyncio.async(run_code(mode,[executable]))
-        task.name = name
-        tasks.append(task)
-    loop.run_until_complete(run(tasks))
-    loop.close()
-    #shutil.rmtree(_session_dir)
+
+    if target is not '__local__':
+        _session.add_target(target)
+        _session.to_dir(local_session_dir)
+        _target = _session.targets[0]
+        remote_session_dir = _session['remote-session-dir']
+        _runtime = runtime.Runtime(_target, local_session_dir, remote_session_dir, loop=loop)
+        try:
+            loop.run_until_complete(_runtime.initialize())
+            loop.run_until_complete(_runtime.run_global_explorers())
+            for key,value in _target['explorer'].items():
+                click.echo('{0}: {1}'.format(key, value))
+        except exceptions.CdistError as e:
+            log.error(str(e))
+            raise
+            ctx.exit(1)
+        finally:
+            loop.close()
+    else:
+        # run explorers on the local machine without going though ssh
+        _session_dir = tempfile.mkdtemp(prefix='cdist-session-')
+        log.debug('session_dir: {0}'.format(_session_dir))
+        _session.to_dir(_session_dir)
+
+        explorer_path = os.path.join(_session_dir, 'conf/explorer')
+        os.environ['__explorer'] = explorer_path
+
+        mode = 'shell'
+        tasks = []
+        for name,executable in _session['conf']['explorer'].items():
+            if explorer and name not in explorer:
+                continue
+            task = asyncio.async(run_code(mode,[executable]))
+            task.name = name
+            tasks.append(task)
+        loop.run_until_complete(run(tasks))
+        loop.close()
+        #shutil.rmtree(_session_dir)
