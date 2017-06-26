cc14ba214a809e0198403d2e903e69ab3441418d
Steven Armstrong <steven.armstrong@id.ethz.ch>
2017-06-26 23:25:58 +0200

    run locally using special Runtime instance; support json output
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- cdist/cli/commands/explore.py
+++ cdist/cli/commands/explore.py
@@ -4,6 +4,8 @@ import tempfile
 import shutil
 import asyncio
 import asyncio.subprocess
+import glob
+import json
 
 import click
 
@@ -14,42 +16,48 @@ from cdist import runtime
 from cdist.cli.utils import comma_delimited_string_to_set
 
 
-async def run_code(mode, code):
-    if mode == 'exec':
-        process = await asyncio.create_subprocess_exec(*code, stdout=asyncio.subprocess.PIPE)
-        stdout, _ = await process.communicate()
-        return stdout
-    elif mode == 'shell':
-        cmd = ' '.join(code)
-        process = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE)
-        stdout, _ = await process.communicate()
-        return stdout
 
+class LocalRuntime(runtime.Runtime):
+    """A Runtime that runs global explorers locally using sh instead of ssh.
+    """
 
-async def run(tasks, timeout=10):
-    try:
-        while tasks:
-            done, pending = await asyncio.wait(
-                tasks, timeout=timeout, return_when=asyncio.FIRST_COMPLETED
-            )
-            if not done:
-                break
-            for t in done:
-                tasks.remove(t)
-                result = t.result()
-                value = result.decode('ascii').rstrip()
-                for line in value.split('\n'):
-                    print('{0}: {1}'.format(t.name, line))
-    except Exception as e:
-        print('got exception:', e, flush=True)
+    async def run_global_explorer(self, name):
+        """Run the given global explorer and return it's output.
+        """
+        env = {
+            '__explorer': self.path['local']['explorer'],
+
+        }
+        explorer = os.path.join(self.path['local']['explorer'], name)
+        result = await self.local.check_output([explorer], env=env)
+        return result.decode('ascii').rstrip()
+
+    async def run_global_explorers(self, explorer_names=None):
+        """Run all global explorers and save their output in the session.
+        """
+        self.log.debug('Running global explorers locally')
+        # execute explorers in parallel
+        tasks = []
+        if not explorer_names:
+            explorer_names = glob.glob1(self.path['local']['explorer'], '*')
+        for name in explorer_names:
+            task = self.loop.create_task(self.run_global_explorer(name))
+            task.name = name
+            tasks.append(task)
+        if tasks:
+            results = await asyncio.gather(*tasks)
+            for index,name in enumerate(explorer_names):
+                self.target['explorer'][name] = results[index]
+            await self.sync_target('explorer')
 
 
 @click.command(name='explore')
 @click.option('-e', '--explorer', multiple=True, callback=comma_delimited_string_to_set,
     help='Run the given explorers instead of all of them.')
+@click.option('-j', '--json', 'json_output', is_flag=True, help='Output result as json instead of text.')
 @click.argument('target', nargs=1, default='__local__')
 @click.pass_context
-def main(ctx, explorer, target):
+def main(ctx, explorer, json_output, target):
     """Explore the given target.
 
     TARGET is expected to be the hostname of the target to work on.
@@ -62,10 +70,6 @@ def main(ctx, explorer, target):
     log.debug('ctx.args: {0}'.format(ctx.args))
     log.debug('ctx.params: {0}'.format(ctx.params))
 
-    #if not target:
-    #    target = []
-    #    log.debug('no target given, nothing to do, fail gracefully')
-    #    sys.exit(0)
     log.debug('target: {0}'.format(target))
 
     # Validate options
@@ -76,43 +80,29 @@ def main(ctx, explorer, target):
 
     local_session_dir = tempfile.mkdtemp(prefix='cdist-session-')
 
+    _session.add_target(target)
+    _session.to_dir(local_session_dir)
+    _target = _session.targets[0]
+    remote_session_dir = _session['remote-session-dir']
+
     loop = asyncio.get_event_loop()
 
-    if target is not '__local__':
-        _session.add_target(target)
-        _session.to_dir(local_session_dir)
-        _target = _session.targets[0]
-        remote_session_dir = _session['remote-session-dir']
-        _runtime = runtime.Runtime(_target, local_session_dir, remote_session_dir, loop=loop)
-        try:
+    try:
+        if target is not '__local__':
+            _runtime = runtime.Runtime(_target, local_session_dir, remote_session_dir, loop=loop)
             loop.run_until_complete(_runtime.initialize())
-            loop.run_until_complete(_runtime.run_global_explorers(explorer_names=explorer))
+        else:
+            _runtime = LocalRuntime(_target, local_session_dir, remote_session_dir, loop=loop)
+        loop.run_until_complete(_runtime.run_global_explorers(explorer_names=explorer))
+        if json_output:
+            click.echo(json.dumps(_target['explorer']))
+        else:
             for name,value in _target['explorer'].items():
                 for line in value.split('\n'):
                     click.echo('{0}: {1}'.format(name, line))
-        except exceptions.CdistError as e:
-            log.error(str(e))
-            raise
-            ctx.exit(1)
-        finally:
-            loop.close()
-    else:
-        # run explorers on the local machine without going though ssh
-        _session_dir = tempfile.mkdtemp(prefix='cdist-session-')
-        log.debug('session_dir: {0}'.format(_session_dir))
-        _session.to_dir(_session_dir)
-
-        explorer_path = os.path.join(_session_dir, 'conf/explorer')
-        os.environ['__explorer'] = explorer_path
-
-        mode = 'shell'
-        tasks = []
-        for name,executable in _session['conf']['explorer'].items():
-            if explorer and name not in explorer:
-                continue
-            task = asyncio.async(run_code(mode,[executable]))
-            task.name = name
-            tasks.append(task)
-        loop.run_until_complete(run(tasks))
+    except exceptions.CdistError as e:
+        log.error(str(e))
+        raise
+        ctx.exit(1)
+    finally:
         loop.close()
-        #shutil.rmtree(_session_dir)
